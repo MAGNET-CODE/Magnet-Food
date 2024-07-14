85795c575be42026982d77e7cc497a71ed1cebca
MAGNET-CODE <sozan1382dastan@gmail.com>
2024-07-14 17:57:46 +0330

    fix-categories

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,53 +1,69 @@
 "use client";
 
 import { Suspense, useEffect, useState } from "react";
-import CategoriesPage from "../../../components/templates/CategoriesPage"
+import CategoriesPage from "../../../components/templates/CategoriesPage";
 
-function Categories({ searchParams }) {
+function CategoriesWrapper({ searchParams }) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const { difficulty, time } = searchParams;
+      try {
+        const { difficulty, time } = searchParams;
 
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}data`);
-      const data = await res.json();
-
-      const filteredData = data.filter((item) => {
-        const difficultyResult = item.details.filter(
-          (detail) => detail.Difficulty && detail.Difficulty === difficulty
-        );
-
-        const timeResult = item.details.filter((detail) => {
-          const cookingTime = detail["Cooking Time"] || "";
-          const [timeDetail] = cookingTime.split(" ");
-
-          if (time === "less" && timeDetail && +timeDetail <= 30) {
-            return detail;
-          } else if (time === "more" && timeDetail && +timeDetail > 30) {
-            return detail;
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}data`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
           }
         });
-        if (time && difficulty && timeResult.length && difficultyResult.length) {
-          return item;
-        } else if (!time && difficulty && difficultyResult.length) {
-          return item;
-        } else if (!difficulty && time && timeResult.length) {
-          return item;
-        }
-      });
 
-      setData(filteredData);
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+
+        const data = await res.json();
+
+        const filteredData = data.filter((item) => {
+          const difficultyResult = item.details.filter(
+            (detail) => detail.Difficulty && detail.Difficulty === difficulty
+          );
+
+          const timeResult = item.details.filter((detail) => {
+            const cookingTime = detail["Cooking Time"] || "";
+            const [timeDetail] = cookingTime.split(" ");
+
+            if (time === "less" && timeDetail && +timeDetail <= 30) {
+              return detail;
+            } else if (time === "more" && timeDetail && +timeDetail > 30) {
+              return detail;
+            }
+          });
+          if (time && difficulty && timeResult.length && difficultyResult.length) {
+            return item;
+          } else if (!time && difficulty && difficultyResult.length) {
+            return item;
+          } else if (!difficulty && time && timeResult.length) {
+            return item;
+          }
+        });
+
+        setData(filteredData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     }
 
     fetchData();
   }, [searchParams]);
 
+  return <CategoriesPage data={data} />;
+}
+
+export default function Categories({ searchParams }) {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <CategoriesPage data={data} />
+      <CategoriesWrapper searchParams={searchParams} />
     </Suspense>
   );
 }
-
-export default Categories;
